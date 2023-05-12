5abc53ca55ae40edc88a7890d5e8120a14413b58
Ender Lopez <est.ealemanl0201@uml.edu.ni>
2023-05-12 17:17:19 -0600

    o

--- src/js/header.js
+++ src/js/header.js
@@ -2,8 +2,7 @@ let header = document.getElementById('header');
 
 header.innerHTML = <header class="flex flex-row justify-between items-center p-4 bg-red">
 <div>
-  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
-    class="w-6 h-6 text-white">
+  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 text-white">
     <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
   </svg>
 
