02632a71fcef22b5d90db1c5596bd3391df39949
Ender Lopez <est.ealemanl0201@uml.edu.ni>
2023-05-12 17:25:11 -0600

    E

--- src/js/header.js
+++ src/js/header.js
@@ -1,10 +1,12 @@
 let header = document.getElementById('header');
 
-header.innerHTML = <header class="flex flex-row justify-between items-center p-4 bg-red">
+header.innerHTML = `
+<header class="flex flex-row justify-between items-center p-4 bg-red">
 <div>
-  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 text-white">
-    <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
-  </svg>
+<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 text-red-600">
+<path stroke-linecap="round" stroke-linejoin="round" d="M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" />
+</svg>
+
 
 </div>
 <div class="text-white">Bienvenidos</div>
@@ -15,4 +17,4 @@ header.innerHTML = <header class="flex flex-row justify-between items-center p-4
       d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
   </svg>
 </div>
-</header>
+</header>`;
